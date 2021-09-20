a1391b83b6e3a20e9b1b6c21d52de0f440c7218f
MortenHansen <nichlash3@gmail.com>
2021-09-20 12:49:51 +0200

    minor corrections

--- Uge39/Parking.cs
+++ Uge39/Parking.cs
@@ -1,7 +1,85 @@
+using System;
+using System.Reflection.Emit;
+using System.Threading;
+
 namespace Uge39
 {
-    public class Entrance
+    public class Parking
     {
-        
+        private readonly Object _entranceBlocked = new object();
+        private readonly Object _exitBlocked = new object();
+        private int isThereSpaceAvailable = 0;
+        private int numberOfMaxThreads = 50;
+        private int activeThreads = 0;
+        private int carNumber = 0;
+        private int maximumNumberOfParkingSpots = 10;
+
+        public void CarStart()
+        {
+            var rand = new Random();
+            while (activeThreads<numberOfMaxThreads)
+            {
+                ThreadStart threadStart = new ThreadStart(Entrance);
+                Thread newthread = new Thread(threadStart);
+                activeThreads++;
+                newthread.Start();
+                newthread.Name = $"car {carNumber}";
+                carNumber++;
+                Console.WriteLine($"{newthread.Name} started and is waiting to go to the entrance");
+                
+                Thread.Sleep(rand.Next(200,1000));
+            }
+                Entrance();
+        }
+
+
+        public void Entrance(){
+            lock (_entranceBlocked)
+            {
+                Checkagain:
+                if (isThereSpaceAvailable < maximumNumberOfParkingSpots)
+                {
+                    Thread thread = Thread.CurrentThread;
+                    Console.WriteLine($"{thread.Name} is going through the entrance");
+                    isThereSpaceAvailable++;
+                }
+                else
+                {
+                    //Thread.Sleep(2000);
+                    goto Checkagain;
+                }
+            }
+            InTheParking();
+            
+
+        }
+
+        public  void InTheParking()
+        {
+            Thread thread = Thread.CurrentThread;
+            Console.WriteLine($"{thread.Name} is parked");
+            var rand = new Random();
+            Thread.Sleep(rand.Next(5000,20000));
+            
+            //time for exit
+            ExitParking();
+        }
+
+        public  void ExitParking()
+        {
+            lock (_exitBlocked)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine($"{thread.Name} about to exit the parking area");
+                Thread.Sleep(200);
+                isThereSpaceAvailable--;
+                activeThreads--;
+                //carNumber--;
+
+            }
+            
+        }
+
+
     }
 }
--- Uge39/Program.cs
+++ Uge39/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Threading;
 
 namespace Uge39
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to europark");
+            var parking = new Parking();
+            parking.CarStart();
         }
+
     }
+
+
 }
